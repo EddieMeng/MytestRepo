3fec787c11949efdecefb27adc396f4846b91870
meng <zshinemeng@icloud.com>
2019-05-06 16:08:13 +0800

    create some files

---
+++ app/src/java/MyClass.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Simon on 2019-05-06.
+//
+
+#include "MyClass.h"
+#include <std.io>
+
+int main() {
+
+}
---
+++ app/src/java/MyClass.h
@@ -0,0 +1,15 @@
+//
+// Created by Simon on 2019-05-06.
+//
+
+#ifndef MYTESTREPO_MYCLASS_H
+#define MYTESTREPO_MYCLASS_H
+
+class MyClass {
+
+
+};
+
+
+
+#endif //MYTESTREPO_MYCLASS_H
